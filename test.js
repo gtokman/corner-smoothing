725d0fbc20e89acd8c9a3c6fcbc85f9961fb1523
Gary Tokman <yo@6ary.com>
2024-11-05 09:35:57 -0500

    fix: update to include files

--- test.js
+++ test.js
@@ -19,6 +19,6 @@ describe('Smooth Corners Plugin', () => {
       utilities['.smooth-corners-md']['@supports (mask-border-width: 50px)'][
         'mask-border'
       ]
-    ).toBe('url("./mask@1x.png") 49% fill / 60px');
+    ).toBe('url("./assets/masks/mask@1x.png") 49% fill / 60px');
   });
 });
